8f38203568e376b6191c2f7ab815d58cc5d2fd8c
Colin Murtaugh <colin_murtaugh@harvard.edu>
2015-05-20 10:51:38 -0400

    added new relic

--- scripts/install_new_relic.sh
+++ scripts/install_new_relic.sh
@@ -4,5 +4,5 @@ rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarc
 yum install -y newrelic-php5 || true
 
 export NR_INSTALL_SILENT=true
-export NR_INSTALL_KEY=your_new_relic_key_here
+export NR_INSTALL_KEY=48498b1ceb7cf1bd68b57b0a985ae7ef63e3a2b2
 newrelic-install install
